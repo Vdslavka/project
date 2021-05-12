ae7cfd11e8bc20be50a146c6798586f968b84348
Vlada <example@gamail.com>
2021-05-12 15:55:35 +0300

    first commit

---
+++ course/script.js
@@ -0,0 +1,18 @@
+"use strict";
+alert('hello')
+//const resolt = confirm('Are you here?')
+//console.log(resolt);
+
+
+//const answer = +prompt('Вам есть 18?', '')
+//console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?');
+answers[1] = prompt('Сколько вам лет?', '');
+answers[2] = prompt('Где вы живете?', '');
+
+//document.write(answers);
+
+console.log(typeof(answers));
